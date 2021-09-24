0073a70851e98beeeddf21a6fbc2c3ef4205e4b8
hassanzaid-bit <hzaid.hz@gmail.com>
2021-09-25 02:36:09 +0300

    Footer complete

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 
 const sidebar = document.getElementById("sidebar");
+const link = document.getElementById("footer-dropdown-id")
+
+const dropdownContent = document.getElementById("footer-dropdown-content")
+
+// const height = document.getElementsByTagName("nav").height();
+
+// console.log(height)
 
 
 function showSidebar(){
     sidebar.classList.toggle("active")
+}
+
+function myfunction(){
+    link.classList.toggle("expanded")
+    dropdownContent.classList.toggle("dropdown-active")
 }
